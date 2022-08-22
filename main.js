8dbf19de229b52043312e22f08e03a85b2304c92
Gonzalo Ballarini <ballarinigonzalo@gmail.com>
2022-08-22 00:41:11 -0300

    Agregando eventos js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import { stockliving } from "./stock.js"
+let modalIsOpen = false;
+let modal = document.getElementById ("modal");
+window.addEventListener('load', ()=> {
+    modal.classList.add ("visibility:hidden");
+} )
+
+//Creacion de las cards de Productos
 
 const mostrarProductos = (stockliving) =>{
     
@@ -9,11 +16,11 @@ const mostrarProductos = (stockliving) =>{
             div.innerHTML +=  ` <div class="card-image">
                                 <img src=${stockliving.img} style= width:100%>
                                 <span class= "card-title">${stockliving.nombre}</span><br>
-                                                                </div>
+                                </div>
                                 <div>
                                 <p>${stockliving.descripcion}</p>
                                 <p>${stockliving.precio}</p>
-                                <a class="btn btn-primary" id=boton${stockliving.id} style= float:right>Comprar</a>
+                                <a class="btn btn-primary" href="#${stockliving.id}" id=boton style= float:right>Comprar</a>
                                 </div>
             `;
             contenedor.appendChild(div);   
@@ -21,5 +28,15 @@ const mostrarProductos = (stockliving) =>{
 }; 
 
 mostrarProductos (stockliving)
+
+//Tengo el problema que solo funciona con el Id nro. 1 no con los demás...probé con un for each pero no me funcionó...
+let btn = document.getElementById ("boton");
+btn.addEventListener ("click", modalfunc)
+function modalfunc (){
+    console.log ("Respuesta al evento"), 
+    prompt ("Usted está a punto de agregar al carrito el producto quiere continuar?"); 
+}
+
+
 
 
